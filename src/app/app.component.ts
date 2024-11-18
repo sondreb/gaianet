6ab62ce9ad66e118f8c9facb7fd11b39fe81f6b5
SondreB <sondre@outlook.com>
2024-11-18 16:19:20 +0100

    Make the Gaianet database app

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  deferredPrompt: any;
+  showInstallButton = false;
+
+  ngOnInit() {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+      this.showInstallButton = true;
+    });
+  }
+
+  async installPwa() {
+    if (!this.deferredPrompt) return;
+    
+    this.deferredPrompt.prompt();
+    const { outcome } = await this.deferredPrompt.userChoice;
+    
+    if (outcome === 'accepted') {
+      this.showInstallButton = false;
+    }
+    this.deferredPrompt = null;
+  }
+}
